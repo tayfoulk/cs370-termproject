1893021a1603a358cc0a76e5b3589bc16c8a4170
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-23 21:47:55 -0600

    Update Encryption.h

--- Encryption.h
+++ Encryption.h
@@ -2,7 +2,23 @@
 #include <math.h>
 
 char *encrypt(char *FileContents, char *key) {
+  char *temp;
+  char *result;
 
+  strcpy (temp, "");
+  
+  for (int i; i < strlen(FileContents); i++) {
+    if (i % 10 != 9) {
+      strcat (temp, FileContents[i] + key[i % strlen(key);
+    } else {
+      strcat (result, shuffle(temp));
+      strcpy (temp, "");
+    }
+  }
+}
+
+char *shuffle(char *input) {
+  
 }
 
 char *decrypt(char *FileContents, char *key) {
