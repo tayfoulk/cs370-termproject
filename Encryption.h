94eaeaa4135b86c8ea2312392d294870af804cf9
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-25 22:28:10 -0600

    Update Encryption.h

--- Encryption.h
+++ Encryption.h
@@ -18,7 +18,20 @@ char *encrypt(char *FileContents, char *key) {
 }
 
 char *shuffle(char *input) {
-  
+  char *temp;
+  strcpy (temp, "");
+
+  strcat(temp, input[7]);
+  strcat(temp, input[0]);
+  strcat(temp, input[8]);
+  strcat(temp, input[6]);
+  strcat(temp, input[1]);
+  strcat(temp, input[4]);
+  strcat(temp, input[9]);
+  strcat(temp, input[2]);
+  strcat(temp, input[3]);
+  strcat(temp, input[5]);
+
 }
 
 char *decrypt(char *FileContents, char *key) {
