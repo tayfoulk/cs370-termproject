7b20b0c72a329b031bff366c5f26bae7daa7d025
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-28 13:38:13 -0600

    Create Encryption.h

---
+++ Encryption.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *encrpypt(char*, char*);
+char* append(char*, char);
+char shiftKeyUp(char, char);
+char shiftKeyDown(char, char);
+char *shuffle(char *);
+char *decrypt(char *, char*);
