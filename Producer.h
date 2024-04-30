1a354eb1d0c1ca0a02e07e762e009df57d9c1501
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-30 16:06:12 -0600

    Update Producer.h

--- Producer.h
+++ Producer.h
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <math.h>
+
 //shared memory
 #include <sys/shm.h>
 #include <unistd.h>
 #include <stdint.h>
+
+// our headers
 #include "Encryption.h"
 #include "convert.h"
 #include "sha1.h"
