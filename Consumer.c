a4dde5f2dec0a44c73610836b2f6bff936742499
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-30 17:28:58 -0600

    Update Consumer.c

--- Consumer.c
+++ Consumer.c
@@ -4,11 +4,10 @@ Arguments:
     1. shared memory name
     2. message
     3. key
-    4. hash to compare
 */
 
 int main (int argc, char *argv[]) {
-    if (argc != 5) {
+    if (argc != 4) {
         exit (-1);
     }
 
@@ -20,23 +19,10 @@ int main (int argc, char *argv[]) {
     strcpy (encrypted, argv[2]);
 
     encrypted = decrypt(encrypted, argv[3]);
-    //hold hash
-    uint32_t hash[5];
 
-    sha1(encrypted, hash);
-    char hash_str[128];
-    //store hashval in hash_str
-    ui32_to_char(hash, hash_str);
 
     uint32_t valread=read(fd, &shmid, sizeof(shmid));
-    answer = (int*)shmat(shmid, NULL, 0);
-
-    *answer = 0;
-    
-    //compare hash_str to argv[4], the provided hash
-    if (strcmp(hash_str, argv[4])) {
-        *answer = 1;
-    }
+    answer = (char*)shmat(shmid, NULL, 0);
 
     close(fd);
     shmdt(answer);
