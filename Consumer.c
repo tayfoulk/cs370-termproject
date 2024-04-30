50543ff888fce7ae52a9c1ee2b14bd3cdf37ee4c
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-04-30 16:51:09 -0600

    Update Consumer.c

--- Consumer.c
+++ Consumer.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-//shared memory
-#include <sys/shm.h>
-#include <unistd.h>
-#include <stdint.h>
-#include "Encryption.h"
-#include "convert.h"
-#include "sha1.h"
-
+#include "Consumer.h"
 /*
 Arguments:
     1. shared memory name
