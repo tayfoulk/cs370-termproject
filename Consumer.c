f38be46710da24bc7e0c7f84f1fc10215314be55
tayfoulk <143132850+tayfoulk@users.noreply.github.com>
2024-05-01 16:28:30 -0600

    Update Consumer.c

--- Consumer.c
+++ Consumer.c
@@ -4,6 +4,7 @@ Arguments:
     1. shared memory name
     2. message
     3. key
+    4. key offset
 */
 
 int main (int argc, char *argv[]) {
@@ -18,7 +19,7 @@ int main (int argc, char *argv[]) {
 
     strcpy (encrypted, argv[2]);
 
-    encrypted = decrypt(encrypted, argv[3]);
+    encrypted = decrypt(encrypted, argv[3], atoi(argv[4]);
 
     uint32_t valread=read(fd, &shmid, sizeof(shmid));
     answer = (char*)shmat(shmid, NULL, 0);
